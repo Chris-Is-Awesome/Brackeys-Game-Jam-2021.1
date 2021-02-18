37476af346c7ce62aec5a91a36f046d40caf0efb
Chris is Awesome <chrisstarkofficial@gmail.com>
2021-02-17 23:33:03 -0800

    Slime control system (still early)

---
+++ Assets/Scripts/Player/SlimeSwitcher.cs
@@ -0,0 +1,14 @@
+/*
+ * Author(s):
+	* Chris is Awesome
+ */
+
+using UnityEngine;
+
+public class SlimeSwitcher : MonoBehaviour
+{
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		//
+	}
+}
