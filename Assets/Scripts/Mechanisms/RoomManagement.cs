0942e70f8ef03814e321b0e92f06e096a330dd6e
GameWyrm <daronan97@hotmail.com>
2021-02-27 22:04:36 -0700

    Lighting stuff

--- Assets/Scripts/Mechanisms/RoomManagement.cs
+++ Assets/Scripts/Mechanisms/RoomManagement.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class RoomManagement : MonoBehaviour
 {
     [SerializeField] bool startLoaded = false;
     [SerializeField] Transform minimumBounds;
     [SerializeField] Transform maximumBounds;
+    [SerializeField] float lightingIntensity=0.5f;
     CameraTargetScript cameraTarget;
+    Light2D globalLight;
 
     // Start is called before the first frame update
     void Start()
     {
         if (!startLoaded) RoomLeft();
         cameraTarget = (CameraTargetScript)FindObjectOfType(typeof(CameraTargetScript));
+        globalLight = GameObject.Find("Main Global Light").GetComponent<Light2D>();
     }
 
     // Update is called once per frame
@@ -33,6 +37,7 @@ public class RoomManagement : MonoBehaviour
         }
         cameraTarget.minBounds = minimumBounds;
         cameraTarget.maxBounds = maximumBounds;
+        globalLight.intensity = lightingIntensity;
     }
 
     public void RoomLeft() //disable everything in the room
